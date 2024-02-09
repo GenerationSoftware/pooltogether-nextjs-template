e24f7f8b971b6f17f1876bb8d2b3fd2bfcffe0d4
Ncookiez <ncukiert@gmail.com>
2024-02-09 16:53:53 -0500

    added navbar + basic styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from '@/components/Navbar'
+import classNames from 'classnames'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -13,9 +15,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        {/* TODO: navbar */}
-        <main className='w-full flex flex-col gap-4 items-center'>{children}</main>
+      <body
+        className={classNames(
+          'w-full flex flex-col items-center bg-pt-bg-purple-darker text-pt-purple-50',
+          inter.className
+        )}
+      >
+        <Navbar className='z-50' />
+        <main className='w-full flex flex-col gap-4 items-center px-4 py-8'>{children}</main>
       </body>
     </html>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames'
+import Image from 'next/image'
+
+interface NavbarProps {
+  className?: string
+}
+
+export const Navbar = (props: NavbarProps) => {
+  const { className } = props
+
+  return (
+    <nav
+      className={classNames(
+        'w-full flex justify-center p-4 bg-inherit',
+        'border-b-2 border-b-pt-purple-700 shadow-2xl',
+        className
+      )}
+    >
+      <div className='w-full max-w-screen-xl flex items-center justify-between'>
+        <Image
+          src='pooltogetherLogo.svg'
+          alt='PoolTogether Logo'
+          width={133}
+          height={52}
+          priority={true}
+        />
+      </div>
+    </nav>
+  )
+}
