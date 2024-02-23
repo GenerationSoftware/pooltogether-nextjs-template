c54374ed63084399c02a2ccccce4422339de249a
Ncookiez <ncukiert@gmail.com>
2024-02-23 16:22:42 -0500

    added vault balances

--- src/components/Vault.tsx
+++ src/components/Vault.tsx
@@ -1,7 +1,16 @@
+import { formatTokenAmount } from '@/utils/formatting'
 import { VaultInfo, Vault as VaultType } from '@generationsoftware/hyperstructure-client-js'
-import { useVault } from '@generationsoftware/hyperstructure-react-hooks'
+import {
+  useUserVaultTokenBalance,
+  useVault,
+  useVaultBalance,
+  useVaultShareData
+} from '@generationsoftware/hyperstructure-react-hooks'
 import classNames from 'classnames'
 import Image from 'next/image'
+import { Address } from 'viem'
+import { useAccount } from 'wagmi'
+import { Loading } from './Loading'
 
 interface VaultProps extends VaultInfo {
   className?: string
@@ -13,9 +22,10 @@ export const Vault = (props: VaultProps) => {
   const vault = useVault({ ...rest })
 
   return (
-    <span className={classNames('flex flex-col gap-4 items-center', className)}>
+    <span className={classNames('flex flex-col gap-4', className)}>
       <VaultHeader vault={vault} />
-      {/* TODO: user balance in vault, if wallet connected */}
+      <VaultUserBalance vault={vault} />
+      <VaultBalance vault={vault} />
       {/* TODO: deposit and withdraw forms/buttons when applicable */}
     </span>
   )
@@ -29,10 +39,14 @@ interface BasicProps {
 const VaultHeader = (props: BasicProps) => {
   const { vault, className } = props
 
+  const { data: shareToken } = useVaultShareData(vault)
+
+  const vaultName = vault.name ?? shareToken?.name
+
   return (
     <div className={classNames('flex gap-2 items-center', className)}>
       <VaultLogo vault={vault} className='w-12' />
-      {/* TODO: vault name */}
+      <span className='font-medium text-xl'>{vaultName ?? <Loading className='h-3' />}</span>
     </div>
   )
 }
@@ -48,5 +62,53 @@ const VaultLogo = (props: BasicProps) => {
       height={24}
       className={className}
     />
+  )
+}
+
+const VaultUserBalance = (props: BasicProps) => {
+  const { vault, className } = props
+
+  const { address: userAddress } = useAccount()
+
+  const { data: token } = useUserVaultTokenBalance(vault, userAddress as Address)
+
+  if (!userAddress) {
+    return <></>
+  }
+
+  const userBalance = !!token ? formatTokenAmount(token.amount, token.decimals) : undefined
+
+  return (
+    <span className={classNames('flex gap-1 items-center', className)}>
+      <span className='text-pt-purple-100'>Your Balance:</span>
+      {!!token ? (
+        <span>
+          {userBalance} {token.symbol}
+        </span>
+      ) : (
+        <Loading className='h-2' />
+      )}
+    </span>
+  )
+}
+
+const VaultBalance = (props: BasicProps) => {
+  const { vault, className } = props
+
+  const { data: token } = useVaultBalance(vault)
+
+  const balance = !!token ? formatTokenAmount(token.amount, token.decimals) : undefined
+
+  return (
+    <span className={classNames('flex gap-1 items-center', className)}>
+      <span className='text-pt-purple-100'>Total Vault Assets:</span>
+      {!!token ? (
+        <span>
+          {balance} {token.symbol}
+        </span>
+      ) : (
+        <Loading className='h-2' />
+      )}
+    </span>
   )
 }
