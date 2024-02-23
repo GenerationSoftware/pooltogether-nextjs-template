8dc8fecffb15aacd65c4ca245c0382ad08d93fa5
Ncookiez <ncukiert@gmail.com>
2024-02-23 16:23:21 -0500

    fixed client hydration error (baka nextjs)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { GrandPrize } from '@components/GrandPrize'
 import { Vault } from '@components/Vault'
 import { VAULT_LIST } from '@constants/config'
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => setIsMounted(true), [])
+
   return (
     <>
       <GrandPrize className='my-12' />
       <div className='flex gap-8 items-center flex-wrap'>
-        {VAULT_LIST.tokens.map((vault) => (
-          <Vault key={`${vault.chainId}-${vault.address}`} {...vault} />
-        ))}
+        {isMounted &&
+          VAULT_LIST.tokens.map((vault) => (
+            <Vault key={`${vault.chainId}-${vault.address}`} {...vault} />
+          ))}
       </div>
     </>
   )
